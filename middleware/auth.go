ca353b5411959153a8037ee0ab5493e7cca13edc
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2020-01-31 17:58:39 +0800

    Implement token auth middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -62,6 +62,8 @@ func (m *Auth) SessionRequired(args ...interface{}) gin.HandlerFunc {
 		session := sessions.Default(c)
 		accessToken := session.Get("access_token")
 		if accessToken == nil && !opt {
+			session.Clear()
+			session.Save()
 			c.Redirect(http.StatusMovedPermanently, redirect)
 			c.Abort()
 			return
@@ -71,42 +73,56 @@ func (m *Auth) SessionRequired(args ...interface{}) gin.HandlerFunc {
 			token, err := jwt.ParseWithClaims(accessToken.(string), claims, func(token *jwt.Token) (interface{}, error) {
 				return []byte(os.Getenv("SECRET_KEY")), nil
 			})
-			if err != nil || !token.Valid {
-				c.Redirect(http.StatusMovedPermanently, redirect)
-				c.Abort()
-				return
-			}
-
-			data := map[string]string{
-				"type":  "Bearer",
-				"token": accessToken.(string),
-			}
-			// https://tutorialedge.net/golang/consuming-restful-api-with-go/
-			payload, _ := json.Marshal(data)
-			resp, err := http.Post(os.Getenv("URL_AUTH"), "application/json", bytes.NewBuffer(payload))
 			if err != nil && !opt {
+				session.Clear()
+				session.Save()
 				c.Redirect(http.StatusMovedPermanently, redirect)
 				c.Abort()
 				return
 			}
-			respData, err := ioutil.ReadAll(resp.Body)
-			if err != nil && !opt {
+
+			if !token.Valid && !opt {
+				session.Clear()
+				session.Save()
 				c.Redirect(http.StatusMovedPermanently, redirect)
 				c.Abort()
 				return
 			}
 
-			var userProfile messaging.UserProfile
-			parser := &util.Parser{}
-			parser.UnmarshalResponse(respData, http.StatusOK, &userProfile)
+			if token.Valid {
+				data := map[string]string{
+					"type":  "Bearer",
+					"token": accessToken.(string),
+				}
+				// https://tutorialedge.net/golang/consuming-restful-api-with-go/
+				payload, _ := json.Marshal(data)
+				resp, err := http.Post(os.Getenv("URL_AUTH"), "application/json", bytes.NewBuffer(payload))
+				if err != nil && !opt {
+					session.Clear()
+					session.Save()
+					c.Redirect(http.StatusMovedPermanently, redirect)
+					c.Abort()
+					return
+				}
+				respData, err := ioutil.ReadAll(resp.Body)
+				if err != nil && !opt {
+					session.Clear()
+					session.Save()
+					c.Redirect(http.StatusMovedPermanently, redirect)
+					c.Abort()
+					return
+				}
 
-			fmt.Println(userProfile)
+				var userProfile messaging.UserProfile
+				parser := &util.Parser{}
+				parser.UnmarshalResponse(respData, http.StatusOK, &userProfile)
 
-			c.Set("user_id", userProfile.ID)
-			alira.ViewData = gin.H{
-				"user_id":    userProfile.ID,
-				"username":   userProfile.Username,
-				"url_logout": fmt.Sprintf("%s?redirect=%s", os.Getenv("URL_LOGOUT"), url),
+				c.Set("user_id", userProfile.ID)
+				alira.ViewData = gin.H{
+					"user_id":    userProfile.ID,
+					"username":   userProfile.Username,
+					"url_logout": fmt.Sprintf("%s?redirect=%s", os.Getenv("URL_LOGOUT"), url),
+				}
 			}
 		}
 		c.Next()
@@ -115,6 +131,132 @@ func (m *Auth) SessionRequired(args ...interface{}) gin.HandlerFunc {
 
 func (m *Auth) TokenRequired(args ...interface{}) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		currentPath := c.Request.URL.Path
+		except := os.Getenv("API_EXCEPT")
+		if except != "" {
+			excepts := strings.Split(except, ";")
 
+			for _, value := range excepts {
+				if c.Request.Method == http.MethodGet {
+					if strings.Index(currentPath, value) > 0 {
+						c.Next()
+						return
+					}
+				} else if currentPath == strings.TrimSpace(value) {
+					c.Next()
+					return
+				}
+			}
+		}
+
+		authorization := c.Request.Header.Get("Authorization")
+
+		if authorization == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"code":   http.StatusUnauthorized,
+				"status": http.StatusText(http.StatusUnauthorized),
+				"error":  "missing authorization token",
+			})
+			return
+		}
+
+		tokens := strings.Split(authorization, " ")
+		if len(tokens) != 2 {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"code":   http.StatusUnauthorized,
+				"status": http.StatusText(http.StatusUnauthorized),
+				"error":  "invalid token",
+			})
+			return
+		}
+
+		var claims jwt.Claims
+		if tokens[0] == "Bearer" {
+			claims = &account.AccessTokenClaims{}
+		} else if tokens[0] == "Refresh" {
+			if currentPath != "/api/alpha/auth/refresh" {
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+					"code":   http.StatusUnauthorized,
+					"status": http.StatusText(http.StatusUnauthorized),
+					"error":  "invalid refresh uri",
+				})
+				return
+			}
+			claims = &account.RefreshTokenClaims{}
+		} else {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"code":   http.StatusUnauthorized,
+				"status": http.StatusText(http.StatusUnauthorized),
+				"error":  "invalid token indentifier",
+			})
+			return
+		}
+
+		tokenString := tokens[1]
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("SECRET_KEY")), nil
+		})
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"code":   http.StatusUnauthorized,
+				"status": http.StatusText(http.StatusUnauthorized),
+				"error":  err.Error(),
+			})
+			return
+		}
+
+		if !token.Valid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"code":   http.StatusUnauthorized,
+				"status": http.StatusText(http.StatusUnauthorized),
+				"error":  "invalid token",
+			})
+			return
+		}
+		if tokens[0] == "Refresh" {
+			if claims.(*account.RefreshTokenClaims).Sub != 1 {
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+					"code":   http.StatusUnauthorized,
+					"status": http.StatusText(http.StatusUnauthorized),
+					"error":  "invalid refresh token",
+				})
+				return
+			}
+		}
+
+		data := map[string]string{
+			"type":  tokens[0],
+			"token": tokenString,
+		}
+		// https://tutorialedge.net/golang/consuming-restful-api-with-go/
+		payload, _ := json.Marshal(data)
+		resp, err := http.Post(os.Getenv("URL_AUTH"), "application/json", bytes.NewBuffer(payload))
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"code":   http.StatusUnauthorized,
+				"status": http.StatusText(http.StatusUnauthorized),
+				"error":  "unable to verify token",
+			})
+			return
+		}
+		respData, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"code":   http.StatusUnauthorized,
+				"status": http.StatusText(http.StatusUnauthorized),
+				"error":  "unable to read detail token",
+			})
+			return
+		}
+		var userProfile messaging.UserProfile
+		parser := &util.Parser{}
+		parser.UnmarshalResponse(respData, http.StatusOK, &userProfile)
+
+		c.Set("user_id", userProfile.ID)
+		if tokens[0] == "Refresh" {
+			c.Set("sub", claims.(*account.RefreshTokenClaims).Sub)
+		}
+		c.Next()
 	}
 }
