05eeca46fabd361d8855195f62f9ae718fa1613e
Ivo Hutasoit <ivohutasoit@users.noreply.github.com>
2020-02-03 13:31:06 +0800

    Add callback for login session

---
+++ middleware/callback.middleware.go
@@ -0,0 +1,59 @@
+package middleware
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"github.com/ivohutasoit/alira-commerce/messaging"
+	"github.com/ivohutasoit/alira/util"
+)
+
+type Callback struct{}
+
+func (m *Callback) WebLoginToken(args ...interface{}) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		callback := c.Query("callback")
+		session := sessions.Default(c)
+
+		if callback != "" {
+			data := map[string]string{
+				"token": callback,
+			}
+			// https://tutorialedge.net/golang/consuming-restful-api-with-go/
+			payload, _ := json.Marshal(data)
+			resp, err := http.Post(fmt.Sprintf("%s%s", os.Getenv("URL_ACCOUNT"), os.Getenv("API_TOKENCALLBACK")),
+				"application/json", bytes.NewBuffer(payload))
+			if err != nil {
+				session.Clear()
+				session.AddFlash("error", err.Error())
+				session.Save()
+				c.Redirect(http.StatusMovedPermanently, "/")
+				return
+			}
+			respData, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				session.Clear()
+				session.AddFlash("error", err.Error())
+				session.Save()
+				c.Redirect(http.StatusMovedPermanently, "/")
+				return
+			}
+			var loggedUser messaging.LoggedUser
+			parser := &util.Parser{}
+			parser.UnmarshalResponse(respData, http.StatusOK, &loggedUser)
+
+			if loggedUser.AccessToken != "" {
+				session.Set("access_token", loggedUser.AccessToken)
+				session.Set("refresh_token", loggedUser.RefreshToken)
+				session.Save()
+			}
+		}
+		c.Next()
+	}
+}
